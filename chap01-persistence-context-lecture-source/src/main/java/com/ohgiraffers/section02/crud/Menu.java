9acc976388d1d85a2a969e3b0d703bd7f6cff06f
YS <sunnyskylove22@gmail.com>
2024-06-04 17:21:56 +0900

    24.06.04 - jpa

---
+++ chap01-persistence-context-lecture-source/src/main/java/com/ohgiraffers/section02/crud/Menu.java
@@ -0,0 +1,92 @@
+package com.ohgiraffers.section02.crud;
+
+import jakarta.persistence.*;
+
+@Entity(name = "section02Menu")
+@Table(name = "tbl_menu")       // 데이터베이스 안에 있는 테이블 필드들이 컬럼으로 하나씩 들어가게 된다.
+public class Menu {
+
+    // pk -> not nul, unique (기본 조건)-> auto increment (우리가 지정한것)
+    @Id //  not nul, unique 만 가지고 있다. (밑에 열쇠 띠용~생긴다!)
+    @GeneratedValue(strategy = GenerateType.IDENTITY)
+    /* 필기. strategy 속성
+    *   - AUTO : 우리가 사용하는 DB 에 따른다.
+    *   - IDENTITY / SEQUENCE : mysql auto_increment 사용 / oracle sequence 사용 (둘다 auto increment 와 같다..?)
+    * */
+    @Column(name = "menu_code")
+    private int menuCode;
+
+    @Column(name = "menu_name")
+    private String menuName;
+
+    @Column(name = "menu_price")
+    private int menuPrice;
+
+    @Column(name = "category_code")
+    private int categoryCode;
+
+    @Column(name = "orderable_status")
+    private String orderableStatus;
+
+    // 같은 패키지 내에 있는 녀석들만 사용 가능 할 수 있다.
+    protected Menu() {}
+
+    public Menu(int menuCode, String menuName, int menuPrice, int categoryCode, String orderableStatus) {
+        this.menuCode = menuCode;
+        this.menuName = menuName;
+        this.menuPrice = menuPrice;
+        this.categoryCode = categoryCode;
+        this.orderableStatus = orderableStatus;
+    }
+
+    public int getMenuCode() {
+        return menuCode;
+    }
+
+    public void setMenuCode(int menuCode) {
+        this.menuCode = menuCode;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public int getMenuPrice() {
+        return menuPrice;
+    }
+
+    public void setMenuPrice(int menuPrice) {
+        this.menuPrice = menuPrice;
+    }
+
+    public int getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+    public String getOrderableStatus() {
+        return orderableStatus;
+    }
+
+    public void setOrderableStatus(String orderableStatus) {
+        this.orderableStatus = orderableStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Menu{" +
+                "menuCode=" + menuCode +
+                ", menuName='" + menuName + '\'' +
+                ", menuPrice=" + menuPrice +
+                ", categoryCode=" + categoryCode +
+                ", orderableStatus='" + orderableStatus + '\'' +
+                '}';
+    }
+}
